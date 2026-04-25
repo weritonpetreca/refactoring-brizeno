5d317786e83e854c23522afce240caca9f175973
Weriton Luis Petreca <eulcf@hotmail.com>
2026-04-25 12:21:03 -0300

    fix(tests): RealWorldApplicationTests now conects to ephemeral postgres and had tests for that

--- real-world-implementations/src/test/java/dev/weriton/patterns/realworld/RealWorldApplicationTests.java
+++ real-world-implementations/src/test/java/dev/weriton/patterns/realworld/RealWorldApplicationTests.java
@@ -2,11 +2,16 @@ package dev.weriton.patterns.realworld;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.ApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DisplayName("Testes de Inicialização do Spring Boot")
 @SpringBootTest
@@ -17,9 +22,25 @@ class RealWorldApplicationTests {
     @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18-alpine");
 
-    @Test
-    @DisplayName("Deve carregar o contexto da aplicação com sucesso")
-    void contextLoads() {
+    @Autowired
+    private ApplicationContext applicationContext;
 
+    @Test
+    @DisplayName("Deve carregar o contexto da aplicação com sucesso e conectar ao banco efêmero")
+    void contextLoads() {
+        assertNotNull(applicationContext, "O contexto do Spring Boot não deveria ser nulo");
+    }
+
+    @Test
+    @DisplayName("Deve executar o método main da aplicação com sucesso")
+    void mainLoads() {
+        assertDoesNotThrow(() -> {
+            RealWorldApplication.main(new String[]{
+                    "--server.port=0",
+                    "--spring.datasource.url=" + postgres.getJdbcUrl(),
+                    "--spring.datasource.username=" + postgres.getUsername(),
+                    "--spring.datasource.password=" + postgres.getPassword()
+            });
+        }, "A execução do método main falhou e lançou uma exceção");
     }
 }
